58e6815194e257d01d0132ecee709945a0d92ecc
ychento <yiyaochen@gmail.com>
2023-09-27 23:51:47 +0000

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,18 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+print "Enter a store name: "
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+
+if new_store.save
+  puts "Store created successfully!"
+else
+  puts "Store creation failed. Errors:"
+  new_store.errors.full_messages.each do |error|
+    puts error
+  end
+end
+
